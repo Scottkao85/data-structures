749bf6e5c820eead7354d66a834f4369caa982b4
scottkao85 <scottkao85@gmail.com>
2015-02-05 10:28:41 -0800

    Removed some debugging

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -5,7 +5,6 @@ var Queue = function() {
   newQueue.last = 0;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-console.log("newQueue: ",newQueue)
   return newQueue;
 };
 
@@ -29,4 +28,3 @@ queueMethods.size = function(){
   return this.last-this.first;
 };
 
-var enqueue = Object.create(queueMethod)
