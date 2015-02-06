3b682763f66c0b42696a6bc161b22043c3133990
scottkao85 <scottkao85@gmail.com>
2015-02-05 21:34:27 -0800

    Progress on Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,9 @@ var Tree = function(value){
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+
+  extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -15,12 +17,37 @@ var Tree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target){
-
+  var result = false;
+  var searchTree = function(target, tree){
+    if (tree.value === target){
+      result = true;
+    // }else if(tree.children.length === 0){
+    //   return false;
+    }else {
+      for(var i = 0; i < tree.children.length; i++){
+        searchTree(target, tree.children[i]);
+      }
+    }
+    searchTree(target, this);
+    return result;
+  };
 };
+
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
+
+// var pine = Tree('Parents');
+
+// pine.value(); // 'Parents'
+
 
 
 /*
