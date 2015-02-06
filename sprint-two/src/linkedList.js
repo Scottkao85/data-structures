f8dccf9035a8a8544aa1640fbec090f4f0fdb2f7
scottkao85 <scottkao85@gmail.com>
2015-02-05 16:42:32 -0800

    LinkedList almost done

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,16 +4,44 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    if (list.head === null && list.tail === null){
+      list.head = Node(value);
+      list.tail = list.head;
+      // list.tail points to the new Node(value) we're creating
+    }else {
+      list.tail.next = Node(value);
+      list.tail = list.tail.next;
+    }
+
+
   };
 
   list.removeHead = function(){
+    if(list.head !== null) {
+      var result = list.head.value;
+      var temp = list.head;
+      list.head = list.head.next;
+      delete temp;
+    }
+    return result;
   };
 
   list.contains = function(target){
+    var searchList = function(target, node) {
+      if( node.next === null ){
+        return false;
+      } else if( node.value === target ){
+        return true;
+      } else {
+        searchList(target, node.next);
+      }
+    };
+    return searchList(target, list.head);
   };
 
   return list;
 };
+
 
 var Node = function(value){
   var node = {};
