5d3f3050db54238f04ac73057be5be273ba3894a
scottkao85 <scottkao85@gmail.com>
2015-02-06 16:31:45 -0800

    finished Graph's edge methods.
    fixed spec runner on checking removeEdge

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,12 +1,16 @@
 
 
 var Graph = function(){
-  this.storage = {};
+  this.storage = {
+
+  };
 };
 
 Graph.prototype.addNode = function(node){
-  this.storage[node] = node;
-  //console.log("Storage: ", this.storage, "Node: ", node);
+  this.storage[node] = {
+    "node" : node,
+    "edges" : {}
+  };
 };
 
 Graph.prototype.contains = function(node){
@@ -14,15 +18,21 @@ Graph.prototype.contains = function(node){
 };
 
 Graph.prototype.removeNode = function(node){
+  delete this.storage[node];
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+  return toNode in this.storage[fromNode].edges;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  this.storage[fromNode].edges[toNode] = toNode;
+  this.storage[toNode].edges[fromNode] = fromNode;
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+  delete this.storage[fromNode].edges[toNode];
+  delete this.storage[toNode].edges[fromNode];
 };
 
 Graph.prototype.forEachNode = function(cb){
@@ -34,6 +44,6 @@ Graph.prototype.forEachNode = function(cb){
 
 
 var test = new Graph();
-
-test.addNode('cat');
+var add = function(a,b){return a+b};
+test.addNode(add);
 
